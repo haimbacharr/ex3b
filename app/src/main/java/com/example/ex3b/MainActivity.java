486893a3afd34e0e94c4d6209b0aced189d02023
haimbacharr <81071510+haimbacharr@users.noreply.github.com>
2022-06-07 11:45:09 +0300

    add comments in all program.

--- app/src/main/java/com/example/ex3b/MainActivity.java
+++ app/src/main/java/com/example/ex3b/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,32 +19,34 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("Main Activity"); // Set the name of the new activity to "Register"
+        setContentView(R.layout.activity_main); // load the main activity.
+        getSupportActionBar().setTitle("Main Activity"); // Set the name of the new activity to "Register"(the second activity).
         tv1 = (TextView) findViewById(R.id.textView);
         registerBtn = (Button) findViewById(R.id.buttonReg);
     }
 
+    //This method will called when we will click in the register button.
     public void registerButton (View view){
-
-        Intent intent = new Intent(this,Register.class);
-        startActivityForResult(intent,REGISTER_ID);
+        Intent intent = new Intent(this,Register.class); //This is an explicit intent because we the target component(Register.class).
+        startActivityForResult(intent,REGISTER_ID); // requires an result from the Register activity.
     }
 
+    //This method handle the result from the Register activity.
+    //data is the intent send by the register activity.
     @Override
-    protected void onActivityResult(int requestCode,int resultCode, Intent intent) {
-        super.onActivityResult(requestCode, resultCode, intent);
-        if (requestCode == REGISTER_ID) {
-            if (resultCode == RESULT_OK) {
-                if (intent.getExtras() != null) {
+    protected void onActivityResult(int requestCode,int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REGISTER_ID) { // check if we get result from the Register activity through the REGISTER_ID.
+            if (resultCode == RESULT_OK) { // now check if the result is valid.
+                if (data.getExtras() != null) { // check if there is some extras in the intent.
                     getSupportActionBar().setTitle("Main Activity with the registeration details"); // Set the name of the new activity to "Register"
                     String gender;
-                    gender = intent.getStringExtra(Register.GENDER);
+                    gender = data.getStringExtra(Register.GENDER);
                     if (gender.equals("Male"))
                         gender = "Mr.";
                     else gender = "Ms.";
-                    tv1.setText("Welcome back " + gender + intent.getStringExtra(Register.FIRST_NAME) + ", " + intent.getStringExtra(Register.LAST_NAME));
-                    registerBtn.setText("again...");
+                    tv1.setText("Welcome back " + gender + data.getStringExtra(Register.FIRST_NAME) + ", " + data.getStringExtra(Register.LAST_NAME));
+                    registerBtn.setText("again..."); // change the name of the register button.
                 }
             }
         }
--- app/src/main/java/com/example/ex3b/Register.java
+++ app/src/main/java/com/example/ex3b/Register.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
 
+    //Those variables will use for key value in map.
     public static final String FIRST_NAME = "first_name";
     public static final String LAST_NAME = "last_name";
     public static final String GENDER = "gender_from_rg";
@@ -26,16 +27,20 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_register); // load the register activity.
         getSupportActionBar().setTitle("Register"); // Set the name of the new activity to "Register"
 
         firstName = (EditText) findViewById(R.id.edFirstName);
         lastName = (EditText) findViewById(R.id.edLastName);
         genderFromGroup = (RadioGroup) findViewById(R.id.rgGender);
+
+        //Create new toasts.
         missingFirstName = Toast.makeText(getApplicationContext(), "First Name field missing", Toast.LENGTH_SHORT);
         missingLastName = Toast.makeText(getApplicationContext(), "Last Name field missing", Toast.LENGTH_SHORT);
         missingRadioButton = Toast.makeText(getApplicationContext(),"Radio button field missing",Toast.LENGTH_SHORT);
     }
+
+    //This method called when we click on the "Send Back" button.
     public void returnBtnClicked (View view) {
         String firstNameStr = firstName.getText().toString();
         String lastNameStr = lastName.getText().toString();
@@ -45,12 +50,14 @@ public class Register extends AppCompatActivity {
         else if ( genderFromGroup.getCheckedRadioButtonId() == -1) missingRadioButton.show();
         else {
             /* Now will create intent for transfer data to previous activity */
-            Intent intent = new Intent();
+            Intent intent = new Intent(); // This will be an implicit intent becuase we dont care who called us.
+
+            //now put extras inside the intent
             intent.putExtra(FIRST_NAME, firstNameStr);
             intent.putExtra(LAST_NAME, lastNameStr);
             intent.putExtra(GENDER, rgGenderChosen.getText().toString());
             setResult(RESULT_OK, intent);
-            finish();
+            finish();// return the answer to the one who ask.
         }
 
     }
